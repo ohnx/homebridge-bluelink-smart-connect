004ff521ca069f708b4c93e5e54cc6d40e4f5c6c
ohnx <me@masonx.ca>
2023-09-23 23:50:22 -0400

    add code
    finally had some time to make it work consistently. shoutout to
    rate limiting.

--- README.md
+++ README.md
@@ -1,169 +1,72 @@
+# Homebridge bluelink smart connect
 
-<p align="center">
+Homebridge plugin for the bluelink smart connect smart thermostat.
+It was installed in my yuppie box in new york city. maybe someone else will
+find it useful?
 
-<img src="https://github.com/homebridge/branding/raw/master/logos/homebridge-wordmark-logo-vertical.png" width="150">
+## usage notes
 
-</p>
+My thermostat is this one, ymmv with a different one.
 
+<img width="975" alt="image" src="https://github.com/ohnx/homebridge-bluelink-smart-connect/assets/6683648/e9ce7f21-b69b-41ba-9a87-edb3f69685eb">
 
-# Homebridge Platform Plugin Template
+To set it up with wifi, you will have to take the thermostat off of the wall - there are plastic clips so it will snap off, sort of.
+There's a code in the back that tells you how to set up wifi.
 
-This is a template Homebridge platform plugin and can be used as a base to help you get started developing your own plugin.
+* Setup guide (how to use wifi): https://braeburnonline.com/sites/braeburn/files/products/manuals/7300%20Setup.pdf
+* User guide: https://braeburnonline.com/sites/braeburn/files/products/manuals/Braeburn%207300%207305%20User%20Manual_0.pdf
+* Installer guide (how to switch to celsius): https://braeburnonline.com/sites/braeburn/files/products/manuals/Braeburn%207300%207305%20Installer%20Guide_0.pdf
 
-This template should be used in conjunction with the [developer documentation](https://developers.homebridge.io/). A full list of all supported service types, and their characteristics is available on this site.
+**You must switch your thermostat to celsius to have this work.**
+Actually this is a lie and you can totally have the homebridge plugin work with a few tweaks,
+but i'm lazy and i use my thermostat in C mode. It's unfortunate that I lose ~1/2 degree of precision
+(the thermostat only supports integer setpoints, for both C and F), but at least i know
+what the numbers mean.
 
-## Clone As Template
+My homebridge config looks like this:
 
-Click the link below to create a new GitHub Repository using this template, or click the *Use This Template* button above.
-
-<span align="center">
-
-### [Create New Repository From Template](https://github.com/homebridge/homebridge-plugin-template/generate)
-
-</span>
-
-## Setup Development Environment
-
-To develop Homebridge plugins you must have Node.js 12 or later installed, and a modern code editor such as [VS Code](https://code.visualstudio.com/). This plugin template uses [TypeScript](https://www.typescriptlang.org/) to make development easier and comes with pre-configured settings for [VS Code](https://code.visualstudio.com/) and ESLint. If you are using VS Code install these extensions:
-
-* [ESLint](https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint)
-
-## Install Development Dependencies
-
-Using a terminal, navigate to the project folder and run this command to install the development dependencies:
-
-```
-npm install
-```
-
-## Update package.json
-
-Open the [`package.json`](./package.json) and change the following attributes:
-
-* `name` - this should be prefixed with `homebridge-` or `@username/homebridge-` and contain no spaces or special characters apart from a dashes
-* `displayName` - this is the "nice" name displayed in the Homebridge UI
-* `repository.url` - Link to your GitHub repo
-* `bugs.url` - Link to your GitHub repo issues page
-
-When you are ready to publish the plugin you should set `private` to false, or remove the attribute entirely.
-
-## Update Plugin Defaults
-
-Open the [`src/settings.ts`](./src/settings.ts) file and change the default values:
-
-* `PLATFORM_NAME` - Set this to be the name of your platform. This is the name of the platform that users will use to register the plugin in the Homebridge `config.json`.
-* `PLUGIN_NAME` - Set this to be the same name you set in the [`package.json`](./package.json) file. 
-
-Open the [`config.schema.json`](./config.schema.json) file and change the following attribute:
-
-* `pluginAlias` - set this to match the `PLATFORM_NAME` you defined in the previous step.
-
-## Build Plugin
-
-TypeScript needs to be compiled into JavaScript before it can run. The following command will compile the contents of your [`src`](./src) directory and put the resulting code into the `dist` folder.
-
-```
-npm run build
-```
-
-## Link To Homebridge
-
-Run this command so your global install of Homebridge can discover the plugin in your development environment:
-
-```
-npm link
-```
-
-You can now start Homebridge, use the `-D` flag so you can see debug log messages in your plugin:
-
-```
-homebridge -D
-```
-
-## Watch For Changes and Build Automatically
-
-If you want to have your code compile automatically as you make changes, and restart Homebridge automatically between changes, you first need to add your plugin as a platform in `~/.homebridge/config.json`:
-```
-{
-...
-    "platforms": [
+```json
+    "accessories": [
         {
-            "name": "Config",
-            "port": 8581,
-            "platform": "config"
-        },
-        {
-            "name": "<PLUGIN_NAME>",
-            //... any other options, as listed in config.schema.json ...
-            "platform": "<PLATFORM_NAME>"
+            "accessory": "BlueLinkThermostat",
+            "name": "BlueLinkThermostat",
+            "username": "my_email@example.com",
+            "password": "password123"
         }
-    ]
-}
+    ],
 ```
 
-and then you can run:
+## development notes
 
-```
-npm run watch
-```
+Everything was reverse engineered based on the bluelink smart connect ios app:
+https://apps.apple.com/us/app/bluelink-smart-connect/id978578562
 
-This will launch an instance of Homebridge in debug mode which will restart every time you make a change to the source code. It will load the config stored in the default location under `~/.homebridge`. You may need to stop other running instances of Homebridge while using this command to prevent conflicts. You can adjust the Homebridge startup command in the [`nodemon.json`](./nodemon.json) file.
+basically the api is this: (all calls go to `sd2.bluelinksmartconnect.com`)
 
-## Customise Plugin
+1. `POST /api/v1/braeburn/rest-auth/login/` with username and password. rx some cookies (session id) plus an auth token.
+    all subsequent requests have this auth token and cookies.
+2. `GET /api/v1/braeburn/devices/`. not strictly necessary - this is just getting the id of the thermostat.
+3. `GET /api/v1/braeburn/devices/<thermostat_id>/` gets the state of the thermostat.
+   i'm not sure what every key means, but here are explanations for a few under `state_data`
+   (the part that's interesting - the other stuff is pretty boring)
 
-You can now start customising the plugin template to suit your requirements.
+    * `Status_01`: temperature, always in fahrenheit * 100 (e.g. 23 C => 7340, 30 C => 8600)
+    * `User_Setting_02`: current heating/cooling setting. 0 = off, 1 = heat, 2 = cool.
+    * `User_Setting_04`: heating target temp, in system temperature.
+    * `User_Setting_05`: cooling target temp, in system temperature.
+    * If i had to guess, 09/10 have something to do with fan mode.
+    * `Installer_Setting_07` and `Installer_Setting_09`: when the pair is `(2, 0)`, the system is using celsius.
+       when the pair is `(3, 1)`, the system is using fahrenheit.
+    * `Installer_Setting_11`: allowed set range, i think? first 2 digits look like they're the max heating, last 2 digits look like they're min coolling.
+      this setting changes depending on C/F.
+4. `POST /api/v1/braeburn/manage/<thermostat_id>/setstateattr` sets specific parameters. example body:
+   ```
+   {"User_Setting_05":"24","User_Setting_04":"16"}
+   ```
 
-* [`src/platform.ts`](./src/platform.ts) - this is where your device setup and discovery should go.
-* [`src/platformAccessory.ts`](./src/platformAccessory.ts) - this is where your accessory control logic should go, you can rename or create multiple instances of this file for each accessory type you need to implement as part of your platform plugin. You can refer to the [developer documentation](https://developers.homebridge.io/) to see what characteristics you need to implement for each service type.
-* [`config.schema.json`](./config.schema.json) - update the config schema to match the config you expect from the user. See the [Plugin Config Schema Documentation](https://developers.homebridge.io/#/config-schema).
+i know this isn't much. there's plenty of cool stuff with geofencing, etc., but i
+just reversed the bare minimum for the functionality that i wanted, where most of
+the actual "smart" temperature control will be done by homekit automations.
 
-## Versioning Your Plugin
-
-Given a version number `MAJOR`.`MINOR`.`PATCH`, such as `1.4.3`, increment the:
-
-1. **MAJOR** version when you make breaking changes to your plugin,
-2. **MINOR** version when you add functionality in a backwards compatible manner, and
-3. **PATCH** version when you make backwards compatible bug fixes.
-
-You can use the `npm version` command to help you with this:
-
-```bash
-# major update / breaking changes
-npm version major
-
-# minor update / new features
-npm version update
-
-# patch / bugfixes
-npm version patch
-```
-
-## Publish Package
-
-When you are ready to publish your plugin to [npm](https://www.npmjs.com/), make sure you have removed the `private` attribute from the [`package.json`](./package.json) file then run:
-
-```
-npm publish
-```
-
-If you are publishing a scoped plugin, i.e. `@username/homebridge-xxx` you will need to add `--access=public` to command the first time you publish.
-
-#### Publishing Beta Versions
-
-You can publish *beta* versions of your plugin for other users to test before you release it to everyone.
-
-```bash
-# create a new pre-release version (eg. 2.1.0-beta.1)
-npm version prepatch --preid beta
-
-# publish to @beta
-npm publish --tag=beta
-```
-
-Users can then install the  *beta* version by appending `@beta` to the install command, for example:
-
-```
-sudo npm install -g homebridge-example-plugin@beta
-```
-
-
+oh also, there's some rate limiting on the api side. not sure about the exact rate,
+but i have some caching in place in order to help avoid running into errors.
---
+++ src/accessory.ts
@@ -0,0 +1,415 @@
+import {
+  AccessoryConfig,
+  AccessoryPlugin,
+  API,
+  CharacteristicEventTypes,
+  CharacteristicGetCallback,
+  CharacteristicSetCallback,
+  CharacteristicValue,
+  HAP,
+  Logging,
+  Service
+} from "homebridge";
+
+import {Mutex, MutexInterface} from 'async-mutex';
+
+const {https} = require('follow-redirects');
+
+/*
+ * IMPORTANT NOTICE
+ *
+ * One thing you need to take care of is, that you never ever ever import anything directly from the "homebridge" module (or the "hap-nodejs" module).
+ * The above import block may seem like, that we do exactly that, but actually those imports are only used for types and interfaces
+ * and will disappear once the code is compiled to Javascript.
+ * In fact you can check that by running `npm run build` and opening the compiled Javascript file in the `dist` folder.
+ * You will notice that the file does not contain a `... = require("homebridge");` statement anywhere in the code.
+ *
+ * The contents of the above import statement MUST ONLY be used for type annotation or accessing things like CONST ENUMS,
+ * which is a special case as they get replaced by the actual value and do not remain as a reference in the compiled code.
+ * Meaning normal enums are bad, const enums can be used.
+ *
+ * You MUST NOT import anything else which remains as a reference in the code, as this will result in
+ * a `... = require("homebridge");` to be compiled into the final Javascript code.
+ * This typically leads to unexpected behavior at runtime, as in many cases it won't be able to find the module
+ * or will import another instance of homebridge causing collisions.
+ *
+ * To mitigate this the {@link API | Homebridge API} exposes the whole suite of HAP-NodeJS inside the `hap` property
+ * of the api object, which can be acquired for example in the initializer function. This reference can be stored
+ * like this for example and used to access all exported variables and classes from HAP-NodeJS.
+ */
+let hap: HAP;
+
+/*
+ * Initializer function called when the plugin is loaded.
+ */
+export = (api: API) => {
+  hap = api.hap;
+  api.registerAccessory("BlueLinkThermostat", BlueLinkThermostat);
+};
+
+class BluelinkApiWrapper {
+  private readonly log: Logging;
+  private readonly host: string = "sd2.bluelinksmartconnect.com";
+  private readonly username: string;
+  private readonly password: string;
+  private auth_token: string | null;
+  private cookie_jar: string[];
+  private device_id: string | null;
+  private cache;
+  private mutex;
+
+  constructor(log, username, password) {
+    this.log = log;
+    this.username = username;
+    this.password = password;
+    
+    this.auth_token = null;
+    this.cookie_jar = [];
+    this.device_id = null;
+    this.cache = {device_state: {time: 0, data: {}}};
+    this.login((resp, err) => {if (!err) this.get_devices(null)});
+    this.mutex = new Mutex();
+  }
+
+  private send_request(endpoint, payload, with_authorization = true, callback) {
+    let opts = {
+        hostname: this.host,
+        path: endpoint,
+        method: payload ? 'POST' : 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'User-Agent': 'Braeburn/13 CFNetwork/1406.0.4 Darwin/22.4.0',
+        }
+    };
+
+    if (with_authorization) {
+      opts.headers['Authorization'] = `Token ${this.auth_token}`;
+      opts.headers['Cookie'] = `${this.cookie_jar.join(' ')}`
+    }
+
+    const req = https.request(opts, (res) => {
+      this.log.info(`${payload ? 'POST' : 'GET'} ${endpoint}: ${res.statusCode}`);
+      let responseData = '';
+      res.on('data', (chunk) => { responseData += chunk; });
+
+      res.on('end', () => {
+        if (callback) {
+          callback({data: responseData, headers: res.headers}, null);
+        }
+      });
+    });
+
+    if (payload) {
+      req.write(JSON.stringify(payload));
+    }
+
+    req.end();
+    req.on('error', (error) => {
+      this.log.error(error);
+      if (callback) {
+        callback(null, error);
+      }
+    });
+  }
+
+  login(callback) {
+    let login_details = {password: this.password, username: this.username};
+    this.send_request('/api/v1/braeburn/rest-auth/login/', login_details, false, (response, err) => {
+      if (err) {
+        if (callback) callback(null, err);
+        return;
+      }
+
+      let {data, headers} = response;
+      this.auth_token = JSON.parse(data).key;
+      this.cookie_jar = headers['set-cookie'].map(x => { x.split(';')[0] });
+
+      if (callback) {
+        callback(true, null);
+      }
+    });
+  }
+
+  get_devices(callback) {
+    this.send_request('/api/v1/braeburn/devices/', null, true, (response, err) => {
+      if (err) {
+        if (callback) callback(null, err);
+        return;
+      }
+
+      let responseData = JSON.parse(response.data);
+      if (responseData.length < 1) {
+        this.log.error("No thermostats in account!");
+        return;
+      } else if (responseData.length > 1) {
+        this.log.error("More than one thermostat in account, will manage first!");
+      }
+
+      this.device_id = responseData[0].uuid;
+      if (callback) callback(responseData[0], null);
+    });
+  }
+
+  get_device_state(callback) {
+    if (!this.device_id) return this.get_devices(callback);
+
+    // use mutex because for some silly reason apple home makes like 10
+    // concurrent requests which kills the whole rate limiting thing
+    this.mutex
+      .acquire()
+      .then(() => {
+      let rn = Date.now();
+      if (rn - this.cache.device_state.time < 5000) {
+        // return cached response
+        this.log.info("using cached mode! (< 5s)");
+        this.mutex.release();
+        if (callback) callback(this.cache.device_state.data, null);
+        return;
+      }
+  
+      this.send_request('/api/v1/braeburn/devices/'+this.device_id, null, true, (response, err) => {
+        if (err) {
+          this.mutex.release();
+          if (callback) callback(null, err);
+          return;
+        }
+  
+        let responseData = JSON.parse(response.data);
+        this.cache.device_state.data = responseData;
+        this.cache.device_state.time = Date.now();
+        this.mutex.release();
+        if (callback) callback(responseData, null);
+      });
+    });
+  }
+
+  set_device_state(state, callback) {
+    this.send_request('/api/v1/braeburn/manage/'+this.device_id+'/setstateattr/', state, true, (response, err) => {
+      if (err) {
+        if (callback) callback(null, err);
+        return;
+      }
+
+      // invalidate the cache
+      this.cache.device_state.time = 0;
+
+      if (callback) callback(true, null);
+    });
+  }
+
+  is_celsius(state) {
+    // 07 = 2, 09 = 0 => celsius
+    if (state['state_data']['Installer_Setting_07'] == '2') {
+      return true;
+    }
+
+    // 07 = 3, 09 = 1 => fahrenheit
+    return false;
+  }
+}
+
+class BlueLinkThermostat implements AccessoryPlugin {
+  private readonly log: Logging;
+  private readonly name: string;
+  private config: AccessoryConfig;
+  private api: API;
+  private bluelink_api: BluelinkApiWrapper;
+  private readonly service: Service;
+  private readonly informationService: Service;
+
+  constructor(log, config, api) {
+      this.log = log;
+      this.config = config;
+      this.api = api;
+
+      log.info("Thermostat starting initializing!");
+
+      // extract name from config
+      this.name = config.name;
+
+      // get username and password
+      if (!config.username || !config.password) {
+        log.error("Please specify Bluelink username and password in the config using keys `username` and `password`");
+      }
+      this.bluelink_api = new BluelinkApiWrapper(log, config.username, config.password);
+
+      // create a new Thermostat service
+      this.service = new hap.Service.Thermostat(this.name);
+
+      // create handlers for required characteristics
+      this.service.getCharacteristic(hap.Characteristic.CurrentHeatingCoolingState)
+        .on("get", this.handleCurrentHeatingCoolingStateGet.bind(this));
+
+      this.service.getCharacteristic(hap.Characteristic.TargetHeatingCoolingState)
+        .on("get", this.handleTargetHeatingCoolingStateGet.bind(this))
+        .on("set", this.handleTargetHeatingCoolingStateSet.bind(this))
+        // no support for auto
+        .setProps({validValues: [0, 1, 2]});
+
+      this.service.getCharacteristic(hap.Characteristic.CurrentTemperature)
+        .on("get", this.handleCurrentTemperatureGet.bind(this));
+
+      this.service.getCharacteristic(hap.Characteristic.TargetTemperature)
+        .on("get", this.handleTargetTemperatureGet.bind(this))
+        .on("set", this.handleTargetTemperatureSet.bind(this))
+        .setProps({minStep: 1});
+
+      this.service.getCharacteristic(hap.Characteristic.TemperatureDisplayUnits)
+        .on("get", this.handleTemperatureDisplayUnitsGet.bind(this))
+        .on("set", this.handleTemperatureDisplayUnitsSet.bind(this));
+
+      // info
+      this.informationService = new hap.Service.AccessoryInformation()
+        .setCharacteristic(hap.Characteristic.Manufacturer, "Braeburn")
+        .setCharacteristic(hap.Characteristic.Model, "BlueLink SmartConnect Thermostat");
+
+      log.info("Thermostat finished initializing!");
+  }
+
+  /**
+   * Handle requests to get the current value of the "Current Heating Cooling State" characteristic
+   */
+  handleCurrentHeatingCoolingStateGet(callback) {
+    this.bluelink_api.get_device_state((state, err) => {
+      if (err) return;
+      this.log.info('CurrentHeatingCoolingState', state['state_data']['User_Setting_02']);
+      switch(state['state_data']['User_Setting_02']) {
+      case '0': callback(null, hap.Characteristic.CurrentHeatingCoolingState.OFF); break;
+      case '1': callback(null, hap.Characteristic.CurrentHeatingCoolingState.HEAT); break;
+      case '2': callback(null, hap.Characteristic.CurrentHeatingCoolingState.COOL); break;
+      default: this.log.error(`bad response from API ${state['state_data']['User_Setting_02']}`);
+      }
+    });
+  }
+
+  /**
+   * Handle requests to get the current value of the "Target Heating Cooling State" characteristic
+   */
+  handleTargetHeatingCoolingStateGet(callback) {
+    this.bluelink_api.get_device_state((state, err) => {
+      if (err) return;
+      this.log.info('TargetHeatingCoolingState', state['state_data']['User_Setting_02']);
+      switch(state['state_data']['User_Setting_02']) {
+      case '0': callback(null, hap.Characteristic.TargetHeatingCoolingState.OFF); break;
+      case '1': callback(null, hap.Characteristic.TargetHeatingCoolingState.HEAT); break;
+      case '2': callback(null, hap.Characteristic.TargetHeatingCoolingState.COOL); break;
+      default: this.log.error(`bad response from API ${state['state_data']['User_Setting_02']}`);
+      }
+    });
+  }
+
+  /**
+   * Handle requests to set the "Target Heating Cooling State" characteristic
+   */
+  handleTargetHeatingCoolingStateSet(value, callback) {
+    this.bluelink_api.set_device_state({"User_Setting_02": `${value}`}, null);
+  }
+
+  /**
+   * Handle requests to get the current value of the "Current Temperature" characteristic
+   */
+  handleCurrentTemperatureGet(callback) {
+    this.bluelink_api.get_device_state((state, err) => {
+      if (err) return;
+      let temp = parseFloat(state['state_data']['Status_01']) / 100;
+
+      if (this.bluelink_api.is_celsius(state)) {
+        // F => C
+        temp = (temp - 32) / 1.8;
+      }
+
+      callback(null, temp);
+    });
+  }
+
+  /**
+   * Handle requests to get the current value of the "Target Temperature" characteristic
+   */
+  handleTargetTemperatureGet(callback) {
+    this.bluelink_api.get_device_state((state, err) => {
+      if (err) return;
+      switch(state['state_data']['User_Setting_02']) {
+      case '0':
+        // copy of get temp code above
+        let temp = parseFloat(state['state_data']['Status_01']) / 100;
+        if (this.bluelink_api.is_celsius(state)) {
+          // F => C
+          temp = (temp - 32) / 1.8;
+        }
+        callback(null, temp);
+        break;
+      case '1': callback(null, state['state_data']['User_Setting_04']); break;
+      case '2': callback(null, state['state_data']['User_Setting_05']); break;
+      default: this.log.error(`bad response from API ${state['state_data']['User_Setting_02']}`);
+      }
+    });
+  }
+
+  /**
+   * Handle requests to set the "Target Temperature" characteristic
+   */
+  handleTargetTemperatureSet(value, callback) {
+    this.log.info('Triggered SET TargetTemperature:', value);
+
+    this.bluelink_api.get_device_state((state, err) => {
+      if (err) return;
+      switch(state['state_data']['User_Setting_02']) {
+      case '0': // system is off... idk??
+        break;
+      case '1': // heat
+        this.bluelink_api.set_device_state({"User_Setting_04":`${value}`}, null);
+        break;
+      case '2': // cool
+        this.bluelink_api.set_device_state({"User_Setting_05":`${value}`}, null);
+        break;
+      default: this.log.error(`bad response from API ${state['state_data']['User_Setting_02']}`);
+      }
+    });
+  }
+
+  /**
+   * Handle requests to get the current value of the "Temperature Display Units" characteristic
+   */
+  handleTemperatureDisplayUnitsGet(callback) {
+    //this.log.info('Triggered GET TemperatureDisplayUnits');
+
+    this.bluelink_api.get_device_state((state, err) => {
+      if (err) return;
+      if (this.bluelink_api.is_celsius(state)) {
+        // 07 = 2, 09 = 0 => celsius
+        callback(null, hap.Characteristic.TemperatureDisplayUnits.CELSIUS);
+      } else {
+        // 07 = 3, 09 = 1 => fahrenheit
+        callback(null, hap.Characteristic.TemperatureDisplayUnits.FAHRENHEIT);
+      }
+    });
+  }
+
+  /**
+   * Handle requests to set the "Temperature Display Units" characteristic
+   */
+  handleTemperatureDisplayUnitsSet(value, callback) {
+    //this.log.info('Triggered SET TemperatureDisplayUnits:', value);
+  }
+
+  /*
+   * This method is optional to implement. It is called when HomeKit ask to identify the accessory.
+   * Typical this only ever happens at the pairing process.
+   */
+  identify(): void {
+    this.log("Identify!");
+  }
+
+  /*
+   * This method is called directly after creation of this instance.
+   * It should return all services which should be added to the accessory.
+   */
+  getServices(): Service[] {
+    return [
+      this.informationService,
+      this.service,
+    ];
+  }
+
+}
